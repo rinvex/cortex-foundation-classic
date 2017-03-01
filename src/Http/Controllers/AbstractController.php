e0fcdad9630559cadb8ac4b08b3c581bce1e59d7
Abdelrahman Omran <me@omranic.com>
2017-03-01 02:18:20 +0200

    Add basic controllers

---
+++ src/Http/Controllers/AbstractController.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * NOTICE OF LICENSE
+ *
+ * Part of the Cortex Foundation Module.
+ *
+ * This source file is subject to The MIT License (MIT)
+ * that is bundled with this package in the LICENSE file.
+ *
+ * Package: Cortex Foundation Module
+ * License: The MIT License (MIT)
+ * Link:    https://rinvex.com
+ */
+
+namespace Cortex\Foundation\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Rinvex\Fort\Traits\GetsMiddleware;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+
+abstract class AbstractController extends Controller
+{
+    use GetsMiddleware;
+    use DispatchesJobs;
+
+    /**
+     * Whitelisted methods.
+     * Array of whitelisted methods which do not need to go through middleware.
+     *
+     * @var array
+     */
+    protected $middlewareWhitelist = [];
+
+    /**
+     * The broker name.
+     *
+     * @var string
+     */
+    protected $broker;
+
+    /**
+     * Get the broker to be used.
+     *
+     * @return string
+     */
+    protected function getBroker()
+    {
+        return $this->broker;
+    }
+}
---
+++ src/Http/Controllers/AuthenticatedController.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * NOTICE OF LICENSE
+ *
+ * Part of the Cortex Foundation Module.
+ *
+ * This source file is subject to The MIT License (MIT)
+ * that is bundled with this package in the LICENSE file.
+ *
+ * Package: Cortex Foundation Module
+ * License: The MIT License (MIT)
+ * Link:    https://rinvex.com
+ */
+
+namespace Cortex\Foundation\Http\Controllers;
+
+class AuthenticatedController extends AbstractController
+{
+    /**
+     * Create a new manage persistence controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware($this->getAuthMiddleware(), ['except' => $this->middlewareWhitelist]);
+    }
+}
