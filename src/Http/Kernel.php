efc3767631e6dbe8b5ca3fa5955cde51b744e087
Abdelrahman Omran <me@omranic.com>
2017-03-01 02:00:47 +0200

    Install fideloper/proxy package

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Cortex\Foundation\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Fideloper\Proxy\TrustProxies::class,
     ];
 
     /**
