dbdf9ef1ead5cbaf02e6dcc76b0785b9c77f43df
Abdelrahman Omran <me@omranic.com>
2017-03-24 05:05:34 +0200

    Tweak authenticated middleware redirection

--- src/Http/Middleware/RedirectIfAuthenticated.php
+++ src/Http/Middleware/RedirectIfAuthenticated.php
@@ -34,7 +34,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return intend([
+                'url' => '/',
+                'with' => ['success' => trans('cortex/fort::messages.auth.already')],
+            ]);
         }
 
         return $next($request);
