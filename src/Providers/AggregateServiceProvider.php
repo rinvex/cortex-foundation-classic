ef55118fb6b98b318576a2e3349c57e6176abd33
Abdelrahman Omran <me@omranic.com>
2017-04-08 21:45:30 +0200

    Register attributable service provider

--- src/Providers/AggregateServiceProvider.php
+++ src/Providers/AggregateServiceProvider.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 namespace Cortex\Foundation\Providers;
 
 use Rinvex\Modulable\ModulableServiceProvider;
+use Rinvex\Attributable\Providers\AttributableServiceProvider;
 use Illuminate\Support\AggregateServiceProvider as BaseAggregateServiceProvider;
 
 class AggregateServiceProvider extends BaseAggregateServiceProvider
@@ -30,6 +31,7 @@ class AggregateServiceProvider extends BaseAggregateServiceProvider
     protected $providers = [
         ModulableServiceProvider::class,
         FoundationServiceProvider::class,
+        AttributableServiceProvider::class,
         ConsoleSupportServiceProvider::class,
     ];
 }
