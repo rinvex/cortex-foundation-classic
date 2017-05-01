48857202edcc98deb095d35cead7a4913fad9faa
Abdelrahman Omran <me@omranic.com>
2017-05-01 14:43:24 +0200

    Move common frontend views from cortex/fort

---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <strong>{{ config('app.copyright') }}.</strong> All rights reserved.
+    </div>
+</footer>
---
+++ resources/views/frontend/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header class="main-header">
+    <nav class="navbar navbar-static-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="{{ route('frontend.home') }}" class="navbar-brand">{{ config('app.name') }}</a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    {!! app('menus.topbar')->implode('') !!}
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
