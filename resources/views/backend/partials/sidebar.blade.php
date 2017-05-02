85c5a54144926b03d03738d90334562b05eb8cd5
Abdelrahman Omran <me@omranic.com>
2017-05-02 04:54:58 +0200

    Tweak how menus works

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -3,7 +3,7 @@
     <section class="sidebar">
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
-            {!! app('menus.sidebar')->implode('') !!}
+            {!! app('menus.sidebar')->flatten()->implode('') !!}
         </ul>
     </section>
     <!-- /.sidebar -->
