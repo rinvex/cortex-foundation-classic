ab51262829c22be7105644840a513db7e979008d
Abdelrahman Omran <me@omranic.com>
2017-06-11 13:56:49 +0200

    Enforce consistency

--- resources/views/backend/partials/datatable.blade.php
+++ resources/views/backend/partials/datatable.blade.php
@@ -22,7 +22,7 @@
         <!-- Main content -->
         <section class="content">
             <div class="row">
-                <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="col-md-12">
                     <div class="box">
                         <div class="box-header">
                             <h3 class="box-title">
