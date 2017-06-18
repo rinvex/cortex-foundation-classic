b8c19f0580b02febc9eed195cec54ba31c9658c5
Abdelrahman Omran <me@omranic.com>
2017-06-18 17:02:07 +0200

    Replace Auth facade usage with auth() helper

--- src/Http/Middleware/Authenticate.php
+++ src/Http/Middleware/Authenticate.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Cortex\Foundation\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
 class Authenticate
 {
@@ -20,7 +19,7 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->guest()) {
+        if (auth()->guard($guard)->guest()) {
             return intend([
                 'url' => route('frontend.auth.login'),
                 'with' => ['warning' => trans('cortex/foundation::messages.session_required')],
