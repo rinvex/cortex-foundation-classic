0c544ea94b8b90b9191c54bff2130c6da0024b32
Abdelrahman Omran <me@omranic.com>
2017-08-06 17:25:00 +0200

    Add new scripts stack for future vendor usage

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -28,6 +28,7 @@
     <!-- JavaScripts -->
     <script src="{{ mix('js/manifest.js') }}" type="text/javascript"></script>
     <script src="{{ mix('js/vendor.js') }}" type="text/javascript"></script>
+    @stack('scripts-vendor')
     <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
     @stack('scripts')
 
