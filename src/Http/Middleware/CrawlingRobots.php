04a108e27290b7c68f08dfc852c1bab808cb3845
Abdelrahman Omran <me@omranic.com>
2017-08-06 17:23:44 +0200

    Add CrawlingRobots middleware

---
+++ src/Http/Middleware/CrawlingRobots.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Foundation\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+
+class CrawlingRobots
+{
+    protected $response;
+
+    public function handle(Request $request, Closure $next)
+    {
+        $this->response = $next($request);
+        $shouldIndex = $this->shouldIndex($request);
+
+        if (is_bool($shouldIndex)) {
+            return $this->responseWithRobots($shouldIndex ? 'all' : 'none');
+        }
+
+        if (is_string($shouldIndex)) {
+            return $this->responseWithRobots($shouldIndex);
+        }
+
+        throw new Exception('An indexing rule needs to return a boolean or a string.');
+    }
+
+    protected function responseWithRobots(string $contents)
+    {
+        $this->response->headers->set('x-robots-tag', $contents, false);
+
+        return $this->response;
+    }
+
+    /**
+     * @return string|bool
+     */
+    protected function shouldIndex(Request $request)
+    {
+        return app()->environment('production') && ! in_array($request->segment(1), ['backend']);
+    }
+}
