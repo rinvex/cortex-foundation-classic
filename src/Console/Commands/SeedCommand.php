c47c31155f4a735217110b2c0f0309ad21d3e458
Abdelrahman Omran <me@omranic.com>
2017-08-08 16:00:59 +0200

    Add global cortex:seed artisan command

---
+++ src/Console/Commands/SeedCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Foundation\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Console\Input\ArrayInput;
+
+class SeedCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cortex:seed';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Seed default Cortex data.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        collect(Artisan::all())->filter(function ($command) {
+            return strpos($command->getName(), 'cortex:seed:') !== false;
+        })->each->run(new ArrayInput([]), $this->output);
+    }
+}
