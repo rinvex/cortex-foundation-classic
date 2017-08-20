f433adfe61541ef4c5fdae0bc4f969c976ee80a7
Abdelrahman Omran <me@omranic.com>
2017-08-20 03:07:36 +0200

    Add new menu section names

--- resources/lang/en/menus.php
+++ resources/lang/en/menus.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 return [
 
+    'access' => 'Access Control',
+    'taxonomies' => 'Taxonomies',
     'resources' => 'Resources',
     'tools' => 'Tools',
+    'users' => 'User Management',
 
 ];
