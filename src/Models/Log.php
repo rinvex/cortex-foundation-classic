a0c96131c0a2a6bbf84e00fb05bdc4a71d1b9b1a
Abdelrahman Omran <me@omranic.com>
2017-08-22 20:05:27 +0200

    Update models docblock

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -8,6 +8,38 @@ use Watson\Validating\ValidatingTrait;
 use Rinvex\Cacheable\CacheableEloquent;
 use Spatie\Activitylog\Models\Activity;
 
+/**
+ * Cortex\Foundation\Models\Log.
+ *
+ * @property int                                                $id
+ * @property string                                             $log_name
+ * @property string                                             $description
+ * @property int|null                                           $subject_id
+ * @property string|null                                        $subject_type
+ * @property int|null                                           $causer_id
+ * @property string|null                                        $causer_type
+ * @property \Illuminate\Support\Collection                     $properties
+ * @property \Carbon\Carbon                                     $created_at
+ * @property \Carbon\Carbon                                     $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $causer
+ * @property-read mixed                                         $changes
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $subject
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Activitylog\Models\Activity causedBy(\Illuminate\Database\Eloquent\Model $causer)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Activitylog\Models\Activity forSubject(\Illuminate\Database\Eloquent\Model $subject)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Activitylog\Models\Activity inLog($logNames)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereCauserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereCauserType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereLogName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereProperties($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereSubjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereSubjectType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Cortex\Foundation\Models\Log whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Log extends Activity
 {
     use ValidatingTrait;
