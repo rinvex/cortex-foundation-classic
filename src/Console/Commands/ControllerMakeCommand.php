08456ad545b296ea049046b7e91bb3e9679dd1f0
Abdelrahman Omran <me@omranic.com>
2017-09-12 15:12:39 +0200

    Override controller stubs

--- src/Console/Commands/ControllerMakeCommand.php
+++ src/Console/Commands/ControllerMakeCommand.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Console\ControllerMakeCommand as BaseControllerMakeComman
 class ControllerMakeCommand extends BaseControllerMakeCommand
 {
     use ConsoleMakeModuleCommand;
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        if ($this->option('parent')) {
+            return __DIR__.'/../../../resources/stubs/controller.nested.stub';
+        } elseif ($this->option('model')) {
+            return __DIR__.'/../../../resources/stubs/controller.model.stub';
+        } elseif ($this->option('resource')) {
+            return __DIR__.'/../../../resources/stubs/controller.stub';
+        }
+
+        return __DIR__.'/../../../resources/stubs/controller.plain.stub';
+    }
 }
