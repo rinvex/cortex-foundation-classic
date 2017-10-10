a2596121168d561ca81b8e3e05b36ffef3bd24fd
Abdelrahman Omran <me@omranic.com>
2017-10-10 04:53:14 +0200

    Require tab variable

--- resources/views/adminarea/pages/datatable-logs.blade.php
+++ resources/views/adminarea/pages/datatable-logs.blade.php
@@ -23,7 +23,7 @@
                 <ul class="nav nav-tabs">
                     @section('tabs')
                         <li><a href="{{ route("adminarea.{$type}.edit", [str_singular($type) => $resource]) }}">{{ trans('cortex/foundation::common.details') }}</a></li>
-                        <li class="active"><a href="#{{ $tab ?? 'logs' }}-tab" data-toggle="tab">{{ trans('cortex/foundation::common.logs') }}</a></li>
+                        <li class="active"><a href="#{{ $tab }}-tab" data-toggle="tab">{{ trans('cortex/foundation::common.'.$tab) }}</a></li>
                         @if(method_exists($resource, 'causedActivity')) <li><a href="{{ route("adminarea.{$type}.activities", [str_singular($type) => $resource]) }}">{{ trans('cortex/foundation::common.activities') }}</a></li> @endif
                         <li style="float: right; padding: 5px"><select class="form-control dataTableBuilderLengthChanger" aria-controls="{{ $id }}-table"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select></li>
                     @show
@@ -31,7 +31,7 @@
 
                 <div class="tab-content">
 
-                    <div class="tab-pane active" id="{{ $tab ?? 'logs' }}-tab">
+                    <div class="tab-pane active" id="{{ $tab }}-tab">
 
                         {!! $dataTable->table(['class' => 'table table-striped responsive dataTableBuilder', 'id' => "{$id}"]) !!}
 
@@ -50,4 +50,3 @@
 @push('scripts')
     {!! $dataTable->scripts() !!}
 @endpush
-
