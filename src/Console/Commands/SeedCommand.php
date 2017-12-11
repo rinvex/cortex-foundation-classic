0c43d749b3e4fb61b52222ceba869abe8a20ebca
Abdelrahman Omran <me@omranic.com>
2017-12-11 16:49:32 +0200

    Clean SeedCommand and fix seeding issue

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -33,9 +33,5 @@ class SeedCommand extends Command
     public function handle()
     {
         $this->warn($this->description);
-
-        if ($this->ensureExistingDatabaseTables('rinvex/fort')) {
-            $this->seedResources(app('rinvex.fort.ability'), realpath(__DIR__.'/../../../resources/data/abilities.json'), ['name', 'description', 'policy']);
-        }
     }
 }
