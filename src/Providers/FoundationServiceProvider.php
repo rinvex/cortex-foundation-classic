fbce8e84023a035585f4ae6255e0108c4943adf9
Abdelrahman Omran <me@omranic.com>
2018-01-29 16:42:07 +0200

    Simplify IoC binding

--- src/Providers/FoundationServiceProvider.php
+++ src/Providers/FoundationServiceProvider.php
@@ -262,9 +262,7 @@ class FoundationServiceProvider extends ServiceProvider
     {
         // Register artisan commands
         foreach ($this->commands as $key => $value) {
-            $this->app->singleton($value, function ($app) use ($key) {
-                return new $key();
-            });
+            $this->app->singleton($value, $key);
         }
 
         $this->commands(array_values($this->commands));
