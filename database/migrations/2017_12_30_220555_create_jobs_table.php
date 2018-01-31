fc505abca41a4466a7c35a9bf671bcc6b1ed856b
Abdelrahman Omran <me@omranic.com>
2018-01-31 15:23:28 +0200

    Separate indexes from columns definition

--- database/migrations/2017_12_30_220555_create_jobs_table.php
+++ database/migrations/2017_12_30_220555_create_jobs_table.php
@@ -17,12 +17,15 @@ class CreateJobsTable extends Migration
     {
         Schema::create(config('queue.connections.database.table'), function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('queue')->index();
+            $table->string('queue');
             $table->longText('payload');
             $table->unsignedTinyInteger('attempts');
             $table->unsignedInteger('reserved_at')->nullable();
             $table->unsignedInteger('available_at');
             $table->unsignedInteger('created_at');
+
+            // Indexes
+            $table->index('queue');
         });
     }
 
