3c63966b3942bb4036d75d06474125da111fb946
Abdelrahman Omran <me@omranic.com>
2018-02-10 06:56:00 +0200

    Refactor saved/deleted language phrases

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 return [
 
     'already_authenticated' => 'You are already authenticated!',
+    'resource_saved' => 'Congrats! Requested resource [:resource::id] saved successfully.',
+    'resource_deleted' => 'Done! Requested resource [:resource::id] deleted successfully.',
     'resource_not_found' => 'Sorry! Requested resource [:resource::id] not found!',
     'delete_confirmation_title' => 'Delete Confirmation',
     'delete_confirmation_body' => 'Are you sure you want to delete <strong>:name :type</strong>?',
