3ec42559dc3bb51e0283e238a848bdfab2db9e5b
Abdelrahman Omran <me@omranic.com>
2018-03-28 20:41:18 +0200

    Add missing import views

---
+++ resources/views/managerarea/pages/import.blade.php
@@ -0,0 +1,39 @@
+{{-- Master Layout --}}
+@extends('cortex/foundation::managerarea.layouts.default')
+
+{{-- Page Title --}}
+@section('title')
+    {{ config('app.name') }} » {{ trans('cortex/foundation::common.managerarea') }} » {{ $phrase }} » {{ trans('cortex/foundation::common.import') }}
+@endsection
+
+{{-- Main Content --}}
+@section('content')
+
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>{{ Breadcrumbs::render() }}</h1>
+        </section>
+
+        {{-- Main content --}}
+        <section class="content">
+
+            <div class="nav-tabs-custom">
+                {!! Menu::render("{$tabs}", 'nav-tab') !!}
+
+                <div class="tab-content">
+
+                    <div class="tab-pane active" id="{{ $id }}-tab">
+                        {{ Form::open(['url' => $url, 'class' => 'dropzone', 'id' => "{$id}-dropzone", 'data-dz-accepted-files' => 'application/vnd.ms-excel']) }}
+                            <div class="dz-message" data-dz-message><span>{{ trans('cortex/foundation::common.drop_to_import') }}</span></div>
+                        {{ Form::close() }}
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </div>
+
+@endsection
