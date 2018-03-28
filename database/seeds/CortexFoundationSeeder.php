5cbb11f9c4bc65ec7c0f4c17d3de22696b874502
Abdelrahman Omran <me@omranic.com>
2018-03-28 20:41:09 +0200

    Add missing abilities

--- database/seeds/CortexFoundationSeeder.php
+++ database/seeds/CortexFoundationSeeder.php
@@ -14,11 +14,16 @@ class CortexFoundationSeeder extends Seeder
     public function run()
     {
         Bouncer::allow('admin')->to('access-adminarea');
+        Bouncer::allow('owner')->to('access-managerarea');
 
         Bouncer::allow('admin')->to('list', config('medialibrary.media_model'));
         Bouncer::allow('admin')->to('create', config('medialibrary.media_model'));
         Bouncer::allow('admin')->to('update', config('medialibrary.media_model'));
         Bouncer::allow('admin')->to('delete', config('medialibrary.media_model'));
-        Bouncer::allow('admin')->to('audit', config('medialibrary.media_model'));
+
+        Bouncer::allow('owner')->to('list', config('medialibrary.media_model'));
+        Bouncer::allow('owner')->to('create', config('medialibrary.media_model'));
+        Bouncer::allow('owner')->to('update', config('medialibrary.media_model'));
+        Bouncer::allow('owner')->to('delete', config('medialibrary.media_model'));
     }
 }
