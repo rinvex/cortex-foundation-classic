149df102b911f1c923197ed519aade60c2719d9c
Abdelrahman Omran <me@omranic.com>
2018-03-28 20:42:01 +0200

    Replace tenant name by tenant title in page headers

--- resources/views/managerarea/partials/header.blade.php
+++ resources/views/managerarea/partials/header.blade.php
@@ -4,7 +4,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><i class="fa fa-home"></i></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>{{ $currentTenant->name }}</b></span>
+        <span class="logo-lg"><b>{{ $currentTenant->title }}</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
