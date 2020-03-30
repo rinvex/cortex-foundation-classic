94e9b03e3e8bbfbec517fb2c132e3e4cd426e990
Abdelrahman Omran <me@omranic.com>
2020-03-30 05:15:19 +0400

    Add module webpack options

---
+++ resources/js/webpack.js
@@ -0,0 +1,21 @@
+module.exports = {
+    scanForCssSelectors: [],
+    whitelistPatterns: [],
+    webpackPlugins: [],
+    install: [],
+    copy: [
+        {from: 'app/cortex/foundation/resources/images/', to: 'public/images/'},
+        {from: 'node_modules/bootstrap-sass/assets/fonts/bootstrap/', to: 'public/fonts/bootstrap/'},
+        {from: 'node_modules/font-awesome/fonts/', to: 'public/fonts/fontawesome/'},
+        {from: 'node_modules/tinymce/skins/', to: 'public/tinymce/'},
+    ],
+    mix: {
+        css: [
+            {input: 'app/cortex/foundation/resources/sass/theme-frontarea.scss', output: 'public/css/theme-frontarea.css'},
+            {input: 'app/cortex/foundation/resources/sass/theme-adminarea.scss', output: 'public/css/theme-adminarea.css'},
+            {input: 'app/cortex/foundation/resources/sass/theme-tenantarea.scss', output: 'public/css/theme-tenantarea.css'},
+            {input: 'app/cortex/foundation/resources/sass/theme-managerarea.scss', output: 'public/css/theme-managerarea.css'},
+        ],
+        js: [],
+    },
+};
