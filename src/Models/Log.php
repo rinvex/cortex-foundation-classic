4353f85c95230b418f0c05f0bcfa0dbdf891d9ed
Abdelrahman Omran <me@omranic.com>
2020-05-11 13:57:10 +0400

    Update model validation rules

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -86,7 +86,7 @@ class Log extends Activity
      */
     protected $rules = [
         'log_name' => 'required|string|strip_tags|max:150',
-        'description' => 'nullable|string|strip_tags|max:10000',
+        'description' => 'nullable|string|max:10000',
         'subject_id' => 'nullable|integer',
         'subject_type' => 'nullable|string|strip_tags|max:150',
         'causer_id' => 'nullable|integer',
