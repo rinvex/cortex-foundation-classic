89a467e6847b8b9b5c76230bb916c384d635239f
Abdelrahman Omran <me@omranic.com>
2020-05-24 21:39:58 +0400

    Fire custom model events from CRUD actions

---
+++ src/Traits/FiresCustomModelEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Foundation\Traits;
+
+trait FiresCustomModelEvent
+{
+    /**
+     * Fire a custom model event for the given event.
+     *
+     * @param  string  $event
+     * @param  string  $method
+     *
+     * @return mixed|null
+     */
+    protected function fireCustomModelEvent($event, $method)
+    {
+        if (! isset($this->dispatchesEvents[$event])) {
+            return;
+        }
+
+        $result = static::$dispatcher->$method(new $this->dispatchesEvents[$event]($this, $event));
+
+        if (! is_null($result)) {
+            return $result;
+        }
+    }
+}
