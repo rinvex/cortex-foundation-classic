74e2a1440d2b1db2e809cead109573a661dd5802
Abdelrahman Omran <me@omranic.com>
2020-06-14 21:40:08 +0400

    Add intl-tel-input images to webpack processing

--- resources/js/webpack.mix.js
+++ resources/js/webpack.mix.js
@@ -8,6 +8,8 @@ module.exports = {
         {from: 'node_modules/bootstrap-sass/assets/fonts/bootstrap/', to: 'public/fonts/bootstrap/'},
         {from: 'node_modules/font-awesome/fonts/', to: 'public/fonts/fontawesome/'},
         {from: 'node_modules/tinymce/skins/', to: 'public/tinymce/'},
+        {from: 'node_modules/intl-tel-input/build/img/flags.png', to: 'public/images/'},
+        {from: 'node_modules/intl-tel-input/build/img/flags@2x.png', to: 'public/images/'},
     ],
     mix: {
         css: [
