fc466abc4357d4a45fb6b0823a082fbcece6adda
hendawy <hendaoymaher@yahoo.com>
2020-07-13 15:17:11 +0200

    fix wrong access for app helper

--- src/DataTables/AbstractDataTable.php
+++ src/DataTables/AbstractDataTable.php
@@ -299,7 +299,7 @@ CDATA;
             $model = app($this->model);
             $obscure = property_exists($model, 'obscure') && is_array($model->obscure) ? $model->obscure : config('cortex.foundation.obscure');
 
-            if (in_array($this->app('request.accessarea'), $obscure['areas'])) {
+            if (in_array(app('request.accessarea'), $obscure['areas'])) {
                 $selectedIds = $selectedIds->map(function ($value) {
                     return optional(Hashids::decode($value))[0];
                 });
@@ -334,7 +334,7 @@ CDATA;
             $model = app($this->model);
             $obscure = property_exists($model, 'obscure') && is_array($model->obscure) ? $model->obscure : config('cortex.foundation.obscure');
 
-            if (in_array($this->app('request.accessarea'), $obscure['areas'])) {
+            if (in_array(app('request.accessarea'), $obscure['areas'])) {
                 $selectedIds = $selectedIds->map(function ($value) {
                     return optional(Hashids::decode($value))[0];
                 });
@@ -369,7 +369,7 @@ CDATA;
             $model = app($this->model);
             $obscure = property_exists($model, 'obscure') && is_array($model->obscure) ? $model->obscure : config('cortex.foundation.obscure');
 
-            if (in_array($this->app('request.accessarea'), $obscure['areas'])) {
+            if (in_array(app('request.accessarea'), $obscure['areas'])) {
                 $selectedIds = $selectedIds->map(function ($value) {
                     return optional(Hashids::decode($value))[0];
                 });
