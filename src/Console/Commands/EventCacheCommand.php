e77d992019509bbf81651033d2e4643e9149dad3
Abdelrahman Omran <me@omranic.com>
2020-08-30 01:11:20 +0400

    Fix EventCacheCommand, CastMake, ComponentMake, StubPublish artisan commands

---
+++ src/Console/Commands/EventCacheCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Foundation\Console\Commands;
+
+use Cortex\Foundation\Providers\DiscoveryServiceProvider;
+use Illuminate\Foundation\Console\EventCacheCommand as BaseEventCacheCommand;
+
+class EventCacheCommand extends BaseEventCacheCommand
+{
+    /**
+     * Get all of the events and listeners configured for the application.
+     *
+     * @return array
+     */
+    protected function getEvents()
+    {
+        $events = [];
+
+        foreach ($this->laravel->getProviders(DiscoveryServiceProvider::class) as $provider) {
+            $providerEvents = array_merge_recursive($provider->discoverEvents(), $provider->listens());
+
+            $events[get_class($provider)] = $providerEvents;
+        }
+
+        return $events;
+    }
+}
