1bf5e24fec2a1060ab4b1e7c436a0cf8d13ee2cb
Abdelrahman Omran <me@omranic.com>
2021-02-23 03:54:41 +0400

    Remove useless middleware parameters

--- src/Http/Middleware/DiscoverNavigationRoutes.php
+++ src/Http/Middleware/DiscoverNavigationRoutes.php
@@ -13,11 +13,10 @@ class DiscoverNavigationRoutes
      *
      * @param \Illuminate\Http\Request $request
      * @param \Closure                 $next
-     * @param string|null              $guard
      *
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
         if ($accessarea = $request->accessarea()) {
             $menuFiles = app('files')->glob(app()->path("*/*/routes/menus/{$accessarea}.php"));
