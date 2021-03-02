0182723a914c158fb9cf02b371176e5932152b7e
Abdelrahman Omran <me@omranic.com>
2021-03-02 04:03:44 +0400

    Fix setting `auth.defaults.provider` config option

--- src/Http/Middleware/SetAuthDefaults.php
+++ src/Http/Middleware/SetAuthDefaults.php
@@ -23,7 +23,7 @@ class SetAuthDefaults
             // It's better to set auth defaults config globally,
             // instead of using `auth()->shouldUse($guard);`
             config()->set('auth.defaults.guard', $guard);
-            config()->set('auth.defaults.provider', Str::plural($guard));
+            config()->set('auth.defaults.provider', Str::afterLast(Str::plural($guard), ':'));
             config()->set('auth.defaults.passwords', $guard);
             config()->set('auth.defaults.emails', $guard);
         }
