e0d684ff76a64759bb97b45ba649c473bcd1dfc0
Hendawy <hendaoymaher@yahoo.com>
2021-03-08 13:39:52 +0200

    check if route exists before using it

--- resources/views/common/partials/timestamps.blade.php
+++ resources/views/common/partials/timestamps.blade.php
@@ -10,7 +10,13 @@
             <small>
                 <strong>{{ trans('cortex/foundation::common.created_at') }}:</strong>
                 <time datetime="{{ $model->created_at }}">{{ $model->created_at->format(config('app.date_format')) }}</time>
-                @if($model->createdBy && request()->user()->can('update', $model->createdBy)) {{ trans('cortex/foundation::common.by') }} <a href="{{ route(request()->accessarea().'.cortex.auth.'.Str::plural($model->createdBy->getMorphClass()).'.edit', [$model->createdBy->getMorphClass() => $model->createdBy]) }}">{{ $model->createdBy->username }}</a> @endif
+                @if($model->createdBy && request()->user()->can('update', $model->createdBy)) {{ trans('cortex/foundation::common.by') }} 
+                    @if(Route::has(request()->accessarea().'.cortex.auth.'.Str::plural($model->createdBy->getMorphClass()).'.edit'))
+                        <a href="{{ route(request()->accessarea().'.cortex.auth.'.Str::plural($model->createdBy->getMorphClass()).'.edit', [$model->createdBy->getMorphClass() => $model->createdBy]) }}">{{ $model->createdBy->username }}</a> 
+                    @else
+                        {{ $model->createdBy->username }}
+                    @endif
+                @endif
             </small>
         @endif
 
@@ -20,7 +26,13 @@
             <small>
                 <strong>{{ trans('cortex/foundation::common.updated_at') }}:</strong>
                 <time datetime="{{ $model->updated_at }}">{{ $model->updated_at->format(config('app.date_format')) }}</time>
-                @if($model->updatedBy && request()->user()->can('update', $model->updatedBy)) {{ trans('cortex/foundation::common.by') }} <a href="{{ route(request()->accessarea().'.cortex.auth.'.Str::plural($model->updatedBy->getMorphClass()).'.edit', [$model->updatedBy->getMorphClass() => $model->updatedBy]) }}">{{ $model->updatedBy->username }}</a> @endif
+                @if($model->updatedBy && request()->user()->can('update', $model->updatedBy)) {{ trans('cortex/foundation::common.by') }} 
+                    @if(Route::has(request()->accessarea().'.cortex.auth.'.Str::plural($model->updatedBy->getMorphClass()).'.edit' ))
+                        <a href="{{ route(request()->accessarea().'.cortex.auth.'.Str::plural($model->createdBy->getMorphClass()).'.edit', [$model->createdBy->getMorphClass() => $model->createdBy]) }}">{{ $model->createdBy->username }}</a> 
+                    @else
+                        {{ $model->createdBy->username }}
+                    @endif
+                @endif
             </small>
         @endif
     @endif
