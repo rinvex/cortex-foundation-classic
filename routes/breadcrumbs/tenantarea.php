c6d42bd86c4777796cc2b1ee9232620fa9db67fe
Abdelrahman Omran <me@omranic.com>
2021-05-25 17:52:58 +0400

    Replace deprecated `Breadcrumbs::register` with `Breadcrumbs::for`

--- routes/breadcrumbs/tenantarea.php
+++ routes/breadcrumbs/tenantarea.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 use Diglactic\Breadcrumbs\Generator;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 
-Breadcrumbs::register('tenantarea.home', function (Generator $breadcrumbs) {
+Breadcrumbs::for('tenantarea.home', function (Generator $breadcrumbs) {
     $breadcrumbs->push('<i class="fa fa-home"></i> '.config('app.name'), route('tenantarea.home'));
 });
