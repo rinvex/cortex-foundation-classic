827c48c49f1fe6a0ee3d841917f3f2f2d716b1a0
Abdelrahman Omran <me@omranic.com>
2021-08-05 00:11:42 +0400

    Simplify route prefixes

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -6,7 +6,7 @@ Route::domain(domain())->group(function () {
     Route::name('adminarea.')
          ->namespace('Cortex\Foundation\Http\Controllers\Adminarea')
          ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
-         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.adminarea') : config('cortex.foundation.route.prefix.adminarea'))->group(function () {
+         ->prefix(route_prefix('adminarea'))->group(function () {
 
             // Adminarea Home route
              Route::get('/')->name('home')->uses('HomeController@index');
--- routes/web/frontarea.php
+++ routes/web/frontarea.php
@@ -6,7 +6,7 @@ Route::domain(domain())->group(function () {
     Route::name('frontarea.')
          ->middleware(['web'])
          ->namespace('Cortex\Foundation\Http\Controllers\Frontarea')
-         ->prefix(config('cortex.foundation.route.locale_prefix') ? '{locale}/'.config('cortex.foundation.route.prefix.frontarea') : config('cortex.foundation.route.prefix.frontarea'))->group(function () {
+         ->prefix(route_prefix('frontarea'))->group(function () {
 
             // Homepage Routes
              Route::get('/')->name('home')->uses('HomeController@index');
