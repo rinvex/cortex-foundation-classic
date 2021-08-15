0976cc35a40ed96944073584d674e3529866c676
Abdelrahman Omran <me@omranic.com>
2021-08-15 18:21:08 +0400

    Fix wrong accessarea

--- routes/web/tenantarea.php
+++ routes/web/tenantarea.php
@@ -6,7 +6,7 @@ Route::domain('{tenant_domain}')->group(function () {
     Route::name('tenantarea.')
          ->middleware(['web', 'nohttpcache'])
          ->namespace('Cortex\Foundation\Http\Controllers\Tenantarea')
-         ->prefix(route_prefix('frontarea'))->group(function () {
+         ->prefix(route_prefix('tenantarea'))->group(function () {
 
             // Homepage Routes
              Route::get('/')->name('home')->uses('HomeController@index');
