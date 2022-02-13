2f63058685de45daa439a400f91e00093a1314cc
Abdelrahman Omran <me@omranic.com>
2022-02-13 22:54:11 +0400

    Clean migrations

--- database/migrations/2021_01_01_000003_create_queue_job_records_table.php
+++ database/migrations/2021_01_01_000003_create_queue_job_records_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQueueJobsTable extends Migration
+class CreateQueueJobRecordsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2021_01_01_000004_create_queue_job_failures_table.php
+++ database/migrations/2021_01_01_000004_create_queue_job_failures_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFailedQueueJobsTable extends Migration
+class CreateQueueJobFailuresTable extends Migration
 {
     /**
      * Run the migrations.
