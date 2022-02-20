de94d9c60eda2ba81ae9b596442e9edefbc5b100
Abdelrahman Omran <me@omranic.com>
2022-02-20 06:58:09 +0400

    Override Datatables DataArrayTransformer to export columns with their db names instead of their localized titles

---
+++ src/Transformers/DataArrayTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Foundation\Transformers;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Yajra\DataTables\Transformers\DataArrayTransformer as BaseDataArrayTransformer;
+
+class DataArrayTransformer extends BaseDataArrayTransformer
+{
+    /**
+     * Transform row column by collection.
+     *
+     * @override to return headers as db column names, instead of their language titles.
+     *
+     * @param array                          $row
+     * @param \Illuminate\Support\Collection $columns
+     * @param string                         $type
+     *
+     * @return array
+     */
+    protected function buildColumnByCollection(array $row, Collection $columns, $type = 'printable')
+    {
+        $results = [];
+
+        foreach ($columns->all() as $column) {
+            if ($column[$type] && in_array($column['name'], request()->get('visible_columns', []))) {
+                $title = $column['name'];
+                $data = Arr::get($row, $column['data']);
+
+                if ($type == 'exportable') {
+                    $title = $this->decodeContent($title);
+                    $dataType = gettype($data);
+                    $data = $this->decodeContent($data);
+                    settype($data, $dataType);
+                }
+
+                $results[$title] = $data;
+            }
+        }
+
+        return $results;
+    }
+}
