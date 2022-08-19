bf5b326067545891c1a339b3ae7a15357e2f7707
Abdelrahman Omran <me@omranic.com>
2022-08-19 23:21:46 +0400

    Apply tenantable global scope on Eloquent Presence Verifier (i.e. used in validation rules like unique & exists)

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Cortex\Foundation\Models;
 
+use Rinvex\Tenants\Traits\Tenantable;
 use Illuminate\Database\Eloquent\Model;
 
 class AbstractModel extends Model
 {
-    //
+    use Tenantable;
 }
