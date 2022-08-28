5685c9be5428d5e5159adf53963c2cc85aebc80a
Abdelrahman Omran <me@omranic.com>
2022-08-28 08:42:07 +0400

    Refactor EloquentPresenceVerifier and override the default Validator service

---
+++ src/Validators/Validator.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cortex\Foundation\Validators;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Cortex\Foundation\Models\AbstractModel;
+use Illuminate\Validation\Validator as BaseValidator;
+
+/**
+ * @override default validator to support the new `EloquentPresenceVerifier`
+ */
+class Validator extends BaseValidator
+{
+    /**
+     * Parse the connection / table for the unique / exists rules.
+     *
+     * @param string $table
+     *
+     * @return array
+     */
+    public function parseTable($table)
+    {
+        [$connection, $table] = str_contains($table, '.') ? explode('.', $table, 2) : [null, $table];
+
+        if (str_contains($table, '\\') && class_exists($table) && is_a($table, Model::class, true)) {
+            $model = new $table;
+
+            $table = $model->getTable();
+            $connection ??= $model->getConnectionName();
+
+            if (str_contains($table, '.') && Str::startsWith($table, $connection)) {
+                $connection = null;
+            }
+
+            $idColumn = $model->getKeyName();
+        }
+
+        return [$connection, $model ?? (new AbstractModel)->setTable($table), $idColumn ?? null];
+    }
+
+}
--- src/Verifiers/EloquentPresenceVerifier.php
+++ src/Verifiers/EloquentPresenceVerifier.php
@@ -4,38 +4,14 @@ declare(strict_types=1);
 
 namespace Cortex\Foundation\Verifiers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\DatabasePresenceVerifier;
-use Illuminate\Database\ConnectionResolverInterface;
 
 class EloquentPresenceVerifier extends DatabasePresenceVerifier
 {
     /**
-     * The eloquent model instance.
-     *
-     * @var \Illuminate\Database\Eloquent\Model
-     */
-    protected $model;
-
-    /**
-     * Create a new eloquent presence verifier instance.
-     *
-     * @param \Illuminate\Database\ConnectionResolverInterface $db
-     * @param \Illuminate\Database\Eloquent\Model              $model
-     *
-     * @return void
-     */
-    public function __construct(ConnectionResolverInterface $db, Model $model)
-    {
-        parent::__construct($db);
-
-        $this->model = $model;
-    }
-
-    /**
      * Count the number of objects in a collection having the given value.
      *
-     * @param string      $collection
+     * @param string      $model
      * @param string      $column
      * @param string      $value
      * @param int|null    $excludeId
@@ -44,9 +20,9 @@ class EloquentPresenceVerifier extends DatabasePresenceVerifier
      *
      * @return int
      */
-    public function getCount($collection, $column, $value, $excludeId = null, $idColumn = null, array $extra = [])
+    public function getCount($model, $column, $value, $excludeId = null, $idColumn = null, array $extra = [])
     {
-        $query = $this->model($collection)->where($column, '=', $value);
+        $query = $this->model($model)->where($column, '=', $value);
 
         if (! is_null($excludeId) && $excludeId !== 'NULL') {
             $query->where($idColumn ?: 'id', '<>', $excludeId);
@@ -58,16 +34,16 @@ class EloquentPresenceVerifier extends DatabasePresenceVerifier
     /**
      * Count the number of objects in a collection with the given values.
      *
-     * @param string $collection
+     * @param string $model
      * @param string $column
      * @param array  $values
      * @param array  $extra
      *
      * @return int
      */
-    public function getMultiCount($collection, $column, array $values, array $extra = [])
+    public function getMultiCount($model, $column, array $values, array $extra = [])
     {
-        $query = $this->model($collection)->whereIn($column, $values);
+        $query = $this->model($model)->whereIn($column, $values);
 
         return $this->addConditions($query, $extra)->distinct()->count($column);
     }
@@ -75,12 +51,12 @@ class EloquentPresenceVerifier extends DatabasePresenceVerifier
     /**
      * Get model eloquent builder.
      *
-     * @param $collection
+     * @param $model
      *
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    private function model($collection)
+    private function model($model)
     {
-        return $this->model->setConnection($this->connection)->setTable($collection)->useWritePdo();
+        return $model->setConnection($this->connection)->useWritePdo();
     }
 }
