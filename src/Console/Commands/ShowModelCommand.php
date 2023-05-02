f57ba64d8584a29df737e3d7b59d1205eb3801ee
Abdelrahman Omran <me@omranic.com>
2023-05-02 06:10:48 +0400

    Laravel v10: fix update ShowModelCommand console command namespace

--- src/Console/Commands/ShowModelCommand.php
+++ src/Console/Commands/ShowModelCommand.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Cortex\Foundation\Console\Commands;
 
 use Symfony\Component\Console\Attribute\AsCommand;
-use Illuminate\Foundation\Console\ShowModelCommand as BaseShowModelCommand;
+use Illuminate\Database\Console\ShowModelCommand as BaseShowModelCommand;
 
 #[AsCommand(name: 'model:show')]
 class ShowModelCommand extends BaseShowModelCommand
